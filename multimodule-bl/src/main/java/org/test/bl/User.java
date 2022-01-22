befdbb5e145891135e9570767bb1d55444b82ddc
DanilSlim <daniilyakovlev76@gmail.com>
2022-01-22 13:41:15 +0300

    Maven multimodule simple project

---
+++ multimodule-bl/src/main/java/org/test/bl/User.java
@@ -0,0 +1,42 @@
+package org.test.bl;
+
+public class User {
+	
+	private long id;
+	
+	private String name;
+	
+	public User() {
+		
+	}
+	
+	public User(long id, String name) {
+		
+		this.id=id;
+		this.name=name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+
+}
---
+++ multimodule-client/src/main/java/org/test/client/Client.java
@@ -0,0 +1,15 @@
+package org.test.client;
+
+import org.test.bl.User;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		User user=new User(10L,"Pitty");
+		
+		System.out.println(user);
+
+	}
+
+}
